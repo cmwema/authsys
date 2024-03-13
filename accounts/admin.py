b504babbcca28ffba02c7698097cbd6611c26b7b
cmwema <calebmwema65@gmail.com>
2024-03-13 13:43:33 +0300

    create Custom user Model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['email','first_name', 'last_name',
+                    'is_staff', 'is_superuser', 'is_verified', 'is_active',
+                    'date_joined', 'last_login']
+
+
+admin.site.register(User, UserAdmin)
